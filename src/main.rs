21fd531dd4b24d3b4776df0f709d219e02d9665a
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2022-10-24 17:52:13 +0900

    Support mounting app under a certain path

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,14 @@ async fn main() {
         .route("/", get(|| async { "Hello, World!" }))
         .layer(TraceLayer::new_for_http());
 
+    let app_path = std::env::var("APP_PATH").unwrap_or("/".into());
+    let app = if app_path == "/" {
+        app
+    }
+    else {
+        Router::new().nest(&app_path, app)
+    };
+
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
