026ea631229bf05fb187c933f77ad6067be2890d
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2022-10-24 17:35:58 +0900

    Add tracing (logging) feature

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router,
+};
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .with(tracing_subscriber::EnvFilter::from_default_env())
+        .init();
+
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .layer(TraceLayer::new_for_http());
+
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
